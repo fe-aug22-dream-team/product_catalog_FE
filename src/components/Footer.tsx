4bb72c6dc0ec3bce024c42d2b78a977ca8b1ed15
Vladimir Dyblia <vladimirdublya@gmail.com>
2022-12-01 10:18:35 +0200

    added three files

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,9 @@
 import styles from '../styles/footer.module.css';
-import Image from 'next/image'
-import logo from '../icons/Logo.svg';
+import Image from 'next/image';
+import Link from 'next/link';
+import logo from '../../public/icons/Logo.svg';
 import { FC } from 'react';
-import footerButton from '../icons/FooterButton.svg';
+import footerButton from '../../public/icons/FooterButton.svg';
 
 export const Footer: FC = () => {
   return (
@@ -13,18 +14,23 @@ export const Footer: FC = () => {
         alt="Logo"
       />
       <div className={styles.footer_inside}>
-        <a className={styles.footer_links}>GITHUB</a>
-        <a className={styles.footer_links}>CONTACTS</a>
-        <a className={styles.footer_links}>RIGHTS</a>
+        <Link href='https://github.com/fe-aug22-dream-team/product_catalog_fe' className={styles.footer_links}>GITHUB</Link>
+        <Link href='#Contacts' className={styles.footer_links}>CONTACTS</Link>
+        <Link href='#Rights' className={styles.footer_links}>RIGHTS</Link>
       </div>
       <div className={styles.footer_button_container}>
         <p className={styles.footer_back_link}>Back to top</p>
-        <button type='button' className={styles.footer_button}>
-          <Image 
-            src={footerButton}
-            className={styles.footer_button_image}
-            alt='Image for footer button'
-          /> 
+        <button 
+          type='button' 
+          className={styles.footer_button}
+        >
+          <a href="#header">
+            <Image 
+              src={footerButton}
+              className={styles.footer_button_image}
+              alt='Image for footer button'
+            /> 
+          </a>
         </button>
       </div>
     </footer>
